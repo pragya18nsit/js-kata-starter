833cc6991b7b5f1d5ea6d724d05cffb410d99e3f
Pragya Gupta <pragya18nsit@gmail.com>
2021-06-03 15:26:57 +0100

    addead failing test case for creating a new character

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,15 @@
-import { systemUnderTest } from ".";
+import { Character, systemUnderTest } from ".";
 
 describe("Test of systemUnderTest()", function() {
   it("Should pass", function() {
     expect(systemUnderTest()).toEqual(true);
+  });
+});
+
+
+describe("when a new character is created", function() {
+  it("Should have health of 1000", function() {
+    const character = new Character();
+    expect(character.getHealth()).toEqual(1000);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export function systemUnderTest(): boolean {
   return true;
 }
+
+export class Character{
+  getHealth(): any {
+    throw new Error("Method not implemented.");
+  }
+};
